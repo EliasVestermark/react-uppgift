eaf8154fef76abbda066b4ebfff617126fe20f8b
Elias Vestermark <eliasvester96@gmail.com>
2023-10-25 11:53:25 +0200

    lagt till section map

---
+++ src/components/MapSection/MapSection.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Map.css'
+
+const MapSection = () => {
+  return (
+    <section className="map"></section>
+  )
+}
+
+export default MapSection
--- src/views/Contact.js
+++ src/views/Contact.js
@@ -3,12 +3,14 @@ import React from 'react'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 import LetsConnectSection from '../components/LetsConnectSection/LetsConnectSection'
+import Map from '../components/MapSection/MapSection'
 
 const contact = () => {
   return (
     <div className='wrapper'>
       <Header/>
       <LetsConnectSection/>
+      <Map/>
       <Footer/>
     </div>
   )
