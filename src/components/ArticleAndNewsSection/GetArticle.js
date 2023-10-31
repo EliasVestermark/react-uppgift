54dd7a0ec01111bc3020973e3f77042b997fa64f
Emma Mellström <eliasvester96@gmail.com>
2023-10-31 12:24:09 +0100

    datumformatering

--- src/components/ArticleAndNewsSection/GetArticle.js
+++ src/components/ArticleAndNewsSection/GetArticle.js
@@ -1,15 +1,19 @@
 import React from 'react'
 
-const GetArticle = ({ imageUrl, category, title, content}) => {
+import Date from './Date'
+
+const GetArticle = ({ imageUrl, category, title, content, published}) => {
+
+    
+
     return (
         <div className="article">
-            <div className="date">
-                <p className="month"><span>25</span><br/>Mar</p>
-            </div>
+            <Date/>
             <img src={imageUrl} alt="En bild"/>
             <h4>{category}</h4>
             <h3>{title}</h3>
             <p className="text">{content}</p>
+            <p>{published}</p>
         </div>
     ) 
 }
--- src/components/OurNewsSection/OurNewsSection.js
+++ src/components/OurNewsSection/OurNewsSection.js
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './OurNews.css'
 
-import img_article1 from '../../assets/images/article1.png'
-import img_article2 from '../../assets/images/article2.png'
-import img_article3 from '../../assets/images/article3.png'
-import Article from '../../components/ArticleAndNewsSection/Article'
+
 import GetArticle from '../../components/ArticleAndNewsSection/GetArticle'
 import SectionTitle from '../Generics/SectionTitle'
 
@@ -20,19 +17,49 @@ const OurNewsSection = () => {
         try {
             const result = await fetch("https://win23-assignment.azurewebsites.net/api/articles")
             setArticles(await result.json())
-            
         }
         catch (error) {
             console.warn(error)
         }
     }
 
-    // const articles = [
-    //     {date: "25", month: "Mar", img: img_article1, alt: "woman in a classroom", category: "Business", title: "How To Use Digitalization In The Classroom", text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto sed hic libero."},
-    //     {date: "17", month: "Mar", img: img_article2, alt: "chat bot", category: "Business", title: "How To Implement Chat GPT In Your Projects", text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto sed hic libero."},
-    //     {date: "13", month: "Mar", img: img_article3, alt: "a book about css", category: "Business", title: "The Guide To Support Modern CSS Design", text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto sed hic libero."}
-    // ]
+    let dateList = []
 
+    for (let date of articleList) {
+        dateList.push(date.published)
+    }
+    console.log(dateList)
+
+    let date1 = dateList[0]
+
+    console.log(date1)
+    
+    let dateString = date1.toString()
+    console.log(dateString.slice(5, 7))
+
+    
+
+    // console.logdate.slice(5, 7)
+
+    // const sliceDates = () => {
+    //     let dateSliced = date1.slice(1, 12)
+    //     console.log(dateSliced)
+    // }
+
+    // sliceDates()
+
+
+
+    
+    // console.log(date1)
+    // // console.log(date1.slice(2, 6)); 
+    // let dateSliced = date1.slice(5, 7)
+    // console.log(dateSliced)
+
+
+    
+
+    
 
     return (
         <section className="our-news container">
@@ -42,13 +69,9 @@ const OurNewsSection = () => {
             <div className="articles-container">
 
                 {articleList.map((article) => (
-                    <GetArticle key={article.id} imageUrl={article.imageUrl} category={article.category} title={article.title} content={article.content} />
+                    <GetArticle key={article.id} imageUrl={article.imageUrl} category={article.category} title={article.title} content={article.content} published={article.published}/>
                 ))}
-                
-                {/* {articles.map((article, index) => (
-                    <Article key={index} date={article.date} month={article.month} img={article.img} alt={article.alt} category={article.category} title={article.title} text={article.text} />
-                ))} */}
-
+               
             </div>
             <div className="navigation">
                 <p className='square'>&lt;</p>
